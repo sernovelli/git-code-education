0053b749d922e5d2c8625955ebaee39961dab1b1
Sérgio Novelli <sergio@sergionovelli.eti.br>
2015-02-28 18:43:54 -0300

    Finalizando arquivo index.php

--- index.php
+++ index.php
@@ -10,5 +10,9 @@
 		<h3>Curso:</h3><p> Trilhando o caminho com PHP</p>
 		
 		<h3>Módulo:</h3><p> Git/github</p>
+
+		<h3>Aluno:</h3><p> Sérgio Novelli</p>
+
+		<h3>S.O.:</h3><p> Linux Fedora 21</p>
 	</body>
 </html>
