0787aa3839e8cfe559e72f5e502461ba3769b735
Sérgio Novelli <sergio@sergionovelli.eti.br>
2015-02-28 18:39:00 -0300

    Dados iniciais do curso

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>
+			Code Education
+		</title>
+	</head>
+
+	<body>
+		<h1>Informações</h1>
+		<h3>Curso:</h3><p> Trilhando o caminho com PHP</p>
+		
+		<h3>Módulo:</h3><p> Git/github</p>
+	</body>
+</html>
